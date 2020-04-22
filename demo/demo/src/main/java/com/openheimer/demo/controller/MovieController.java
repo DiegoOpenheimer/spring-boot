cb92eb3ae527d8f87cdb11d28e7ceb3e5e92f928
Diego Alves Openheimer <diegoop.pa@hotmail.com>
2020-04-22 11:53:35 -0300

    Adicionado end point para fazer a exclusão do filme.

--- demo/demo/src/main/java/com/openheimer/demo/controller/MovieController.java
+++ demo/demo/src/main/java/com/openheimer/demo/controller/MovieController.java
@@ -2,22 +2,21 @@ package com.openheimer.demo.controller;
 
 import javax.validation.Valid;
 
+import com.openheimer.demo.dto.GenericResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.openheimer.demo.dto.MovieDTO;
 import com.openheimer.demo.model.Movie;
 import com.openheimer.demo.model.User;
 import com.openheimer.demo.repository.MovieRepository;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("movies")
@@ -38,6 +37,24 @@ public class MovieController {
 		Movie movie = movieRepository.save(body.convert(user));
 		body.setId(movie.getId());
 		return body;
+	}
+
+	@DeleteMapping("/{id}")
+	public GenericResponse deleteMovie(@PathVariable Integer id) {
+		try {
+			Optional<Movie> movie = movieRepository.findById(id);
+			if (!movie.isPresent()) {
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Movie not found");
+			}
+			User userLogged = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+			if (!movie.get().getUser().getId().equals(userLogged.getId())) {
+				throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Not unauthorized");
+			}
+			movieRepository.deleteById(id);
+			return new GenericResponse("Movie removed with success");
+		} catch (IllegalArgumentException e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "movie not found");
+		}
 	}
 
 }
